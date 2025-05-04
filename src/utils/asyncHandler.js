a4d339b2d29899b3ebc01c352a91b9cc1431b2e4
Caliber619 <caliber619pubg@gmail.com>
2025-05-04 17:21:37 +0530

    utils (Api error and reponse handling)

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+// Method-1
+const asyncHandler = (requestHandler)=>{
+    (req, res, next) => {
+        Promise
+        .resolve(requestHandler(req, res, next))
+        .catch((err)=> next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+/*
+
+// Method-2
+const asyncHandler = (fn) => async(req, res, next) => {
+    try {
+        await fn(req, res, next)        
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+}  // higher order function (function ko further ek aur function me pass krdena)
+
+*/
