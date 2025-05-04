e766f81ab592c12031844f460424032a9597c769
Caliber619 <caliber619pubg@gmail.com>
2025-05-05 02:44:50 +0530

    minor updates

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ const app = express()
             throw error
         })
 
-        app.listen(process.env.PORT, ()=>{
+        app.listen(process.env.PORT || 8000, ()=>{
             console.log(`App is listening on port ${process.env.PORT}`);
         })
 
