1206a5500dcc821f3dec651f5b634ab117b3ea11
Caliber619 <caliber619pubg@gmail.com>
2025-05-11 22:50:23 +0530

    updates

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,11 @@
 // Method-1
 const asyncHandler = (requestHandler)=>{
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise
         .resolve(requestHandler(req, res, next))
-        .catch((err)=> next(err))
-    }
-}
+        .catch((err)=> next(err));
+    };
+};
 
 export {asyncHandler}
 
